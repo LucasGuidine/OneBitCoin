3c637831c58bea30c893603bf96708678d89df20
Soulokaocom <lucasazevedoabrittaguidine@gmail.com>
2022-02-07 11:33:39 -0300

    integração com a API

--- App.js
+++ App.js
@@ -4,6 +4,24 @@ import HistoryGraphic from './src/components/HistoryGraphic';
 import QuatationsList from './src/components/QuotationsList';
 import QuatationsItens from './src/components/QuotationsList/QuatationsItens';
 
+function addZero(number){
+  if(number<=9){
+    return "0" + number
+  }
+  return number
+}
+
+function url(qtdDays){
+  const data = new Date();
+  const listLastDays=qtdDays;
+  const end_date=
+  `${date.getFullYear()}-${addZero(date.getMonth()+1)}-${addZero(date.getDate())}`;
+  date.setDate(data.getDate()-listLastDays);
+  const start_date=
+  `${date.getFullYear()}-${addZero(date.getMonth() + 1)}-${addZero(date.getDate())}`;
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&end=${end_date}`;
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
